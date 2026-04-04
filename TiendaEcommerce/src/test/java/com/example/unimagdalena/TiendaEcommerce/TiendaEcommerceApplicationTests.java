87ea6e948b6d20e78b6518a655420bdbcff2c4c9
Carlos3117 <caoliverosj@unimagdalena.edu.co>
2026-04-04 11:30:07 -0500

    Add files via upload

---
+++ TiendaEcommerce/src/test/java/com/example/unimagdalena/TiendaEcommerce/TiendaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unimagdalena.TiendaEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
