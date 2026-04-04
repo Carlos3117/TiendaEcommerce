f8690288b06b3ee978d453292012f6075ac449bb
Carlos <caoliverosj@unimagdalena.edu.co>
2026-04-04 15:41:53 -0500

    customer class created

---
+++ src/main/java/com/example/unimagdalena/TiendaEcommerce/entities/Customer.java
@@ -0,0 +1,37 @@
+package com.example.unimagdalena.TiendaEcommerce.entities;
+
+import com.example.unimagdalena.TiendaEcommerce.Enums.CustomerStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CustomerStatus status = CustomerStatus.ACTIVE;
+}
