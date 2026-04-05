1dc2bc515ae01775a3bb494ede1bd3cacec83a01
Carlos <caoliverosj@unimagdalena.edu.co>
2026-04-05 17:26:47 -0500

    services interfaces and impl class created

---
+++ src/main/java/com/example/unimagdalena/TiendaEcommerce/services/AddressService.java
@@ -0,0 +1,5 @@
+package com.example.unimagdalena.TiendaEcommerce.services;
+
+public interface AddressService {
+
+}
---
+++ src/main/java/com/example/unimagdalena/TiendaEcommerce/services/OrderServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.unimagdalena.TiendaEcommerce.services;
+
+public class OrderServiceImpl {
+}
