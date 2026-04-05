8e45eb904127190c876c7d077bb13458156c572f
AdrimarV <amvanegas@unimagdalena.edu.co>
2026-04-05 17:14:22 -0500

    linea corregida

--- src/main/java/com/example/unimagdalena/TiendaEcommerce/entities/Order.java
+++ src/main/java/com/example/unimagdalena/TiendaEcommerce/entities/Order.java
@@ -49,8 +49,13 @@ public class Order {
 
     @PrePersist
     public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-        this.status = OrderStatus.CREATED;
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+
+        if (this.status == null) {
+            this.status = OrderStatus.CREATED;
+        }
     }
 
 }
