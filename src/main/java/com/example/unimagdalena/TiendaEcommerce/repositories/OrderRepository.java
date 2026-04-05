d64aa4a4c0185954f142c387fe467b36f42b5be0
Carlos <caoliverosj@unimagdalena.edu.co>
2026-04-05 13:58:47 -0500

    new queries

--- src/main/java/com/example/unimagdalena/TiendaEcommerce/repositories/OrderRepository.java
+++ src/main/java/com/example/unimagdalena/TiendaEcommerce/repositories/OrderRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomer(Customer customer);
-
+    //Filtros combinados, se puede colocar NULL para ignorar filtros.
     @Query("""
 SELECT o FROM Order o
 WHERE (:customer IS NULL OR o.customer = :customer)
@@ -31,5 +31,24 @@ AND (:maxTotal IS NULL OR o.total <= :maxTotal)
             @Param("minTotal") BigDecimal minTotal,
             @Param("maxTotal") BigDecimal maxTotal
     );
+
+    // Ingresos mensuales agrupados con distinción por año.
+
+    @Query("""
+    SELECT FUNCTION('YEAR', o.createdAt),FUNCTION('MONTH', o.createdAt) , SUM(o.total) 
+    FROM Order o
+    GROUP BY FUNCTION('YEAR', o.createdAt) , FUNCTION('MONTH', o.createdAt)
+    ORDER BY FUNCTION('YEAR', o.createdAt) , FUNCTION('MONTH', o.createdAt) 
+""")
+    List<Object[]> getMonthlyIncome();
+
+    //Clientes con mayor facturación
+
+    @Query("""
+    SELECT o.customer, SUM(o.total)
+    FROM Order o 
+    GROUP BY o.customer
+    ORDER BY SUM(o.total) DESC
+""") List<Object[]> findTopSpendingCustomers();
 
 }
