44c48c14b8a68848bcc224fbdf3bfc13c481de19
Carlos Oliveros <caoliverosj@unimagdalena.edu.co>
2026-04-06 19:34:19 -0500

    dtos created

---
+++ src/main/java/com/example/unimagdalena/TiendaEcommerce/dto/AddressDto.java
@@ -0,0 +1,27 @@
+package com.example.unimagdalena.TiendaEcommerce.dto;
+
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+
+public class AddressDto {
+
+    public record CreateAddressRequest(
+
+            @NotBlank(message = "La dirección es obligatoria")
+            String street,
+
+            @NotBlank(message = "La ciudad es obligatoria")
+            String city,
+
+            @NotNull(message = "El cliente es obligatorio")
+            Long customerId
+
+    ) implements Serializable {}
+
+    public record AddressResponse(
+            Long id,
+            String street,
+            String city,
+            Long customerId
+    ) implements Serializable {}
+}
